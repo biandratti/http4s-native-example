ec343230f8cb0c19e5ac05aad3bb2f114a03356c
Biandratti <maxibiandra@gmail.com>
2023-11-05 13:09:13 +0100

    fix format

--- src/main/scala/com/biandratti/service/HelloWorld.scala
+++ src/main/scala/com/biandratti/service/HelloWorld.scala
@@ -7,10 +7,10 @@ import io.circe.{Encoder, Json}
 import org.http4s.EntityEncoder
 import org.http4s.circe.*
 
-trait HelloWorld[F[_]]:
+trait HelloWorld[F[_]] {
   def hello(n: HelloWorld.Name): F[HelloWorld.Greeting]
-
-object HelloWorld:
+}
+object HelloWorld {
   final case class Name(name: String) extends AnyVal
 
   /** More generally you will want to decouple your edge representations from
@@ -18,15 +18,18 @@ object HelloWorld:
    * encoders for your data.
    */
   final case class Greeting(greeting: String) extends AnyVal
-  object Greeting:
+
+  private object Greeting {
     given Encoder[Greeting] = new Encoder[Greeting]:
       final def apply(a: Greeting): Json = Json.obj(
         ("message", Json.fromString(a.greeting))
       )
 
-    given [F[_]]: EntityEncoder[F, Greeting] =
+    given[F[_]]: EntityEncoder[F, Greeting] =
       jsonEncoderOf[F, Greeting]
-
-  def impl[F[_]: Applicative]: HelloWorld[F] = new HelloWorld[F]:
+  }
+  def impl[F[_] : Applicative]: HelloWorld[F] = new HelloWorld[F]:
     def hello(n: HelloWorld.Name): F[HelloWorld.Greeting] =
       Greeting("Hello, " + n.name).pure[F]
+
+}
